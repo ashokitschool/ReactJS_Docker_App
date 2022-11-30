2bbfd53ef9822378a1854417808ada6cf227a8a7
Ashok IT <ashokitschool@gmail.com>
2022-11-30 09:34:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To Ashok IT...!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ashokitech.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Visit Our Website
         </a>
       </header>
     </div>
